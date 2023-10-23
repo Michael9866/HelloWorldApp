7082621df8180d838ffdc55c4a1ad217a8b87d79
Michael9866 <pq86@yandex.ru>
2023-10-23 20:23:24 +0800

    Initial Commit

---
+++ HelloWorldApp 2.02/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorldApp 2.02
+//
+//  Created by Michael Caine on 23.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

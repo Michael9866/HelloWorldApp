dbdbd733462921dd55215782c294a127b6106adc
Michael9866 <pq86@yandex.ru>
2023-10-31 17:33:28 +0800

    1. Setup UI 2. Work with logic

--- HelloWorldApp 2.02/ViewController.swift
+++ HelloWorldApp 2.02/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var greetingLabel: UIView!
+    @IBOutlet weak var greetingButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        greetingLabel.isHidden = true
+        greetingButton.layer.cornerRadius = 10
+        greetingButton.setTitle("Show Greeting", for: .normal)
     }
-
-
+    
+    
+    @IBAction func greetingPressedButton() {
+        greetingLabel.isHidden.toggle()
+/*
+        if greetingLabel.isHidden {
+            greetingButton.setTitle("Show Greeting", for: .normal)
+        } else {
+            greetingButton.setTitle("Hide Greeting", for: .normal)
+        }
+ */
+// more optimised
+        greetingButton.setTitle(greetingLabel.isHidden ? "Show Greeting" : "Hide Greeting", for: .normal)
+    }
 }
+
 
